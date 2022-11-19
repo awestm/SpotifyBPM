eb81a024627f03a128a9bb1351911932310b8f1a
alexwmays <59802340+alexwmays@users.noreply.github.com>
2022-11-19 17:36:24 -0500

    Added client logic with create react app.

--- index.js
+++ index.js
@@ -62,22 +62,17 @@ app.get('/callback', (req, res) => {
         .then(response => {
             if (response.status === 200) {
 
-                const { access_token, token_type } = response.data;
-
-                axios.get('https://api.spotify.com/v1/me', {
-                    headers: {
-                        Authorization: `${token_type} ${access_token}`
-                    }
+                const { access_token, refresh_token, expires_in } = response.data;
+                const queryParams = querystring.stringify({
+                    access_token,
+                    refresh_token,
+                    expires_in,
                 })
-                    .then(response => {
-                        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-                    })
-                    .catch(error => {
-                        res.send(error);
-                    });
+                //redirect to react app
+                res.redirect(`http://localhost:3000?${queryParams}`)
 
             } else {
-                res.send(response);
+                res.redirect(`/?${querystring.stringify({ error: 'invalid_token'})}`)
             }
         })
         .catch(error => {
