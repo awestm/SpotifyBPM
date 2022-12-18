ce528232b8c3f81664207b748045213b2cf126ad
alexwmays <59802340+alexwmays@users.noreply.github.com>
2022-12-18 18:20:25 -0500

    Adding StyledBPM.js

---
+++ client/src/styles/StyledBPM.js
@@ -0,0 +1,14 @@
+function StyledBPM({children}) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {children}
+        </div>
+    );
+}
+export default StyledBPM;
